aa2b7c59961fbd40dabb1da8396e4776bc344307
Chirag-04 <binarycoderz04@gmail.com>
2024-12-18 19:09:01 +0530

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is a github actions tutorial!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Github Action Tutorial by Chirag Maini😎</h1>
       </header>
     </div>
   );
